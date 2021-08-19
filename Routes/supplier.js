2d0f19cbd79fe92c692caae6e2eb20501c479a25
Minuli-Lavanya <minulilavanya@gmail.com>
2021-08-19 21:17:51 +0530

    Implement Add method

--- Routes/supplier.js
+++ Routes/supplier.js
@@ -30,35 +30,35 @@ router.route("/add").post((req, res)=>{
     })
 })
 
-router.route("/").get((req,res)=>{
-    Supplier_Detail.find().then((supplier)=>{
-        res.json(supplier)
-    }).catch((err)=>{
-        console.log(err)
-    })
-})
+// router.route("/").get((req,res)=>{
+//     Supplier_Detail.find().then((supplier)=>{
+//         res.json(supplier)
+//     }).catch((err)=>{
+//         console.log(err)
+//     })
+// })
 
-router.route("/update/:id").put(async (req, res) => {
-    let userId = req.params.id;
-    const {supplier_id, supplier_name, email, nic, gender} = req.body;
+// router.route("/update/:id").put(async (req, res) => {
+//     let userId = req.params.id;
+//     const {supplier_id, supplier_name, email, nic, gender} = req.body;
 
-    const updateSupplier = {
-        supplier_id,
-        supplier_name,
-        email,
-        nic,
-        phone_number,
-        gender
-    }
+//     const updateSupplier = {
+//         supplier_id,
+//         supplier_name,
+//         email,
+//         nic,
+//         phone_number,
+//         gender
+//     }
 
-    const update = await Supplier_Detail.findByIdAndUpdate(userId, updateSupplier).then(() => {
-        res.status(200).send({status: "User updated", user: update})
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).send({status: "Error with updating data"});
-    })
+//     const update = await Supplier_Detail.findByIdAndUpdate(userId, updateSupplier).then(() => {
+//         res.status(200).send({status: "User updated", user: update})
+//     }).catch((err) => {
+//         console.log(err);
+//         res.status(500).send({status: "Error with updating data", error: err.message});
+//     })
 
     
-})
+// })
 
 module.exports = router;
